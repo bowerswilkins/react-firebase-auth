98e67b2a932e26ec2d90b2eaebdbc821ff5b6579
Andrew Loesch <andrew.loesch@bowerswilkins.com>
2019-12-06 10:48:55 -0800

    commit firebase config and update readme

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config = {
+  firebase: {
+    apiKey: "AIzaSyBRUybx27A_NctYS-ox2HmlqAG65PGx8zw",
+    authDomain: "bowers-services.firebaseapp.com",
+    databaseURL: "https://bowers-services.firebaseio.com",
+    projectId: "bowers-services",
+    storageBucket: "",
+    messagingSenderId: "484209374592",
+    appId: "1:484209374592:web:38408d9030aad96d7cfd65"
+  }
+};
+
+export default config
